f8f548c041b8f016cd44f9251d8497c19ea74403
bjavell <31654366+bjavell@users.noreply.github.com>
2019-05-16 10:22:32 +0800

    Add files via upload
    test

---
+++ precache-manifest.d007fb0f066c145b9c4b12902e909cfb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f70211a45bac30fbba512b67498fab3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fcbd45a56fa6c92fd56b",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "fcbd45a56fa6c92fd56b",
+    "url": "/static/js/main.a15c373c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
